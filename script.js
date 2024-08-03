a31a471821570852dc7419b3eac6f4990a73c066
Carlos Henrique <carlos.carlos27082001@gmail.com>
2024-08-03 18:11:14 -0300

    mudando botao de ligar e desligar

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const turnOn = document.getElementById ( 'turnOn');
-const turnOff = document.getElementById ( 'turnOff');
+const turnOnOff = document.getElementById ( 'turnOnOff');
 const lamp = document.getElementById ('lamp');
 
 function isLampBroken () {
@@ -21,8 +20,17 @@ function lampBroken () {
     lamp.src = './img/quebrada.jpg';
 }
 
-turnOn.addEventListener ('click', lampOn);
-turnOff.addEventListener ('click', lampOff);
+function lampOnOff() {
+    if (turnOnOff.textContent == 'Ligar'){
+        lampOn();
+        turnOnOff.textContent = 'Desligar';
+    }else{
+        lampOff();
+        turnOnOff.textContent = 'Ligar';
+    }
+}
+
+turnOnOff.addEventListener ('click', lampOnOff);
 lamp.addEventListener ('mouseover', lampOn);
 lamp.addEventListener ('mouseleave', lampOff);
 lamp.addEventListener ('dblclick', lampBroken);
